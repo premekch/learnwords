a121a35a397e6fa64583c171c2df7f969e71aa91
premek <premysl.chodura@gmail.com>
2026-04-17 16:44:41 +0200

    fix backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,6 +15,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.use(helmet());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
